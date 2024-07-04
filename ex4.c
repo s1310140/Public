3172f0f16e9f0a8a9c20298b409584191402c8ef
s1310140 <s1310140@u-aizu.ac.jp>
2024-07-04 21:33:49 +0900

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int i,die,count=0;
+  char name[];
+
+  printf("What is your name?\n> ");
+  scanf("%s",name);
+  printf("Hello, %s!\nRolling the dice...\n",name);
+  for(i=0; i<)
+}
